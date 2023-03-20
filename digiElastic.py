6418d2115adc700cad1c2a68a06cf45f741fe96b
Seba <s.joray@gmail.com>
2023-03-20 10:38:01 -0300

    hola hola

--- digiElastic.py
+++ digiElastic.py
@@ -4,7 +4,9 @@ import cv2
 import os
 from PIL import Image
 from elasticsearch import Elasticsearch
-from libtiff import TIFF
+from pathlib import Path
+from skimage import io
+
 
 es = Elasticsearch('http://localhost:9200', basic_auth=('seba', 'gemin8'), verify_certs=False)
 
@@ -13,49 +15,54 @@ es = Elasticsearch('http://localhost:9200', basic_auth=('seba', 'gemin8'), verif
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Abrimos la imagen
-path='/Users/User/proyectos/digitalizacion_matriculas/imagenes/nuevas'
-with os.scandir(path) as ficheros:
-    ficheros = [fichero.name for fichero in ficheros if fichero.is_file() and fichero.name.endswith('.tif')]
-
+path='/Users/User/proyectos/digitalizacion_matriculas/imagenes'
+lista=[]
+for ruta, directorios, ficheros in os.walk(path):
+    print(f'\nDirectorio: {ruta}')
+    for fichero in ficheros:
+        lista.append([ruta, fichero])
 nonombres=['COPROPIEDAD', 'DECRETO', 'LEY','DECRETOLEY','BAHIA', 'LOTE', 'TERRENO', 'FEDERAL','ANTECEDENTE', 'PLANO',
 'CANCELACIONES','DPTO','MINISTERIO','HACIENDA','SIGUE','REGISTRO','PROVINCIA','RESTRICCIONES',
 'EDIFICADO','UBICADO','CIUDAD','CALLE','APROBADO','SOBRE','DOMINIO','INTERDICCIONES','CANCELACIONES',
  'EMPLEADA', 'EMPLEADAS', 'EMPLEADO', 'EMPLEADOS','HIJO','HIJOS','HIJA','HIJAS','DEFINITIVA','BUENOS','AIRES',
  'DORSO', 'PROPIEDAD']
 
-
-for imagenes in ficheros:
-    print(imagenes)
+for par in lista:
+    
+ #   print(imagenes)
     preprocess = "thresh"
-    img = cv2.imread(path + "/" + imagenes,1)
-#    cv2.imshow('imagen de la ruta', img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    if (preprocess == "thresh"):
-        gray = cv2.threshold(
-            gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    elif (preprocess == "blur"):
-        gray = cv2.medianBlur(gray, 3)
-    filename = "{}.png".format(os.getpid())
-    cv2.imwrite(filename, gray)
+    fichero = par[0] + "/" + par[1]
+    print('archivo: ', fichero)
 
-    imgtext = Image.open(filename)
+    img = io.imread(fichero)
+#    cv2.imshow('imagen de la ruta', img)
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #if (preprocess == "thresh"):
+    #    gray = cv2.threshold(
+    #        gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    #elif (preprocess == "blur"):
+    #    gray = cv2.medianBlur(gray, 3)
+    #filename = "{}.png".format(os.getpid())
+    #cv2.imwrite(filename, gray)
+
+    #imgtext = Image.open(filename)
 
     # limpio el codigo de caracteres que no corresponden
-    texto1 = pytesseract.image_to_string(imgtext, lang='font_name', config=f'--psm 6 --oem 1')
+    texto1 = pytesseract.image_to_string(img, lang='font_name', config=f'--psm 6 --oem 1')
     print("-----------------------------------------------------------")
     print(texto1)
     texto = texto1.split('Titularidad')
     if len(texto) > 1:
       print('Texto:',texto[1])    
-    
+        texto1=texto[1]
 
-    """es.index(
+    es.index(
         index='matriculas',
         document={
         'path': path,
-        'imagen': imagenes,
+        'imagen': fichero,
         'texto' : texto1
 
-    })"""
+    })
 
 print (es.info())
