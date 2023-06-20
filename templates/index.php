db235708e9e192374c26311287db7e7629ad30c9
Seba <s.joray@gmail.com>
2023-06-20 12:35:21 -0300

    quitar boots para usarlo online

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
+<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
 <body>
-    <h1 style='color:red'>Cadena a buscar:</h1>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>    <h1 style='color:red'>Cadena a buscar:</h1>
     <form action="{{ url_for('matricula')}}" method="post">
     <h2><input type="texto" name="cadena"></input>
         <button>Enviar</button> </h2>
